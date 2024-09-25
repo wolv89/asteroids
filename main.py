d54181d701f980a0dbf11968473d3b7cfdeea38f
wolv89 <peter.delafosse@gmail.com>
2024-09-25 20:33:51 +1000

    Create window

--- main.py
+++ main.py
@@ -5,10 +5,24 @@ import pygame
 
 from constants import *
 
+
+pygame.init()
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+
 def main():
-	print("Starting asteroids!")
-	print(f"Screen width: {SCREEN_WIDTH}")
-	print(f"Screen height: {SCREEN_HEIGHT}")
+
+	while True:
+
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				return
+
+		screen.fill((0, 0, 0))
+
+		pygame.display.flip()
+
 
 
 
